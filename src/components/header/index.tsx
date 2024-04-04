af1e386937e9b6e752cbecd694dce50fdf9bcf37
otabekov-komron <mrOK.blog@gmail.com>
2024-04-04 16:12:08 +0500

    feat: Footer is done

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,93 +1,99 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Select, Input } from "antd";
 
-import { useHooks } from 'hooks'
-import { Arrow2 } from 'assets/images/icons';
-import Logo from 'assets/images/icons/logo.png'
-import SearchIcon from 'assets/images/icons/search-icon.svg'
-import CartIcon from 'assets/images/icons/cart.svg'
-import BurgerIcon from 'assets/images/icons/burger.svg'
-import BackIcon from 'assets/images/icons/back.svg'
+import { useHooks } from "hooks";
+import { Arrow2 } from "assets/images/icons";
+import Logo from "assets/images/icons/logo.png";
+import SearchIcon from "assets/images/icons/search-icon.svg";
+import CartIcon from "assets/images/icons/cart.svg";
+import BurgerIcon from "assets/images/icons/burger.svg";
+import BackIcon from "assets/images/icons/back.svg";
 
-import "./style.scss"
-import "./mobile.scss"
+import "./style.scss";
+import "./mobile.scss";
 
 interface INav {
-  id: number,
-  link: string,
-  title: string
+  id: number;
+  link: string;
+  title: string;
 }
 
 interface ILang {
-  id: number,
-  name: string,
-  value: string
+  id: number;
+  name: string;
+  value: string;
 }
 
 const Header = () => {
-  const { t, get } = useHooks()
+  const { t, get } = useHooks();
   const { Option } = Select;
-  const [navBarState, seacrhBarState] = useState(false)
-  const [mobileSearchModal, showMobileSearchModal] = useState(false)
+  const [navBarState, seacrhBarState] = useState(false);
+  const [mobileSearchModal, showMobileSearchModal] = useState(false);
 
   const navItems: INav[] = [
     {
       id: 1,
       link: "/about",
-      title: "Kompaniya haqida"
+      title: "Kompaniya haqida",
     },
     {
       id: 2,
       link: "/catalog",
-      title: "Katalog"
+      title: "Katalog",
     },
     {
       id: 3,
       link: "/blog",
-      title: "Blog"
+      title: "Blog",
     },
     {
       id: 4,
-      link: "/contacts",
-      title: "Kontakt"
+      link: "/contact",
+      title: "Kontakt",
     },
-  ]
+  ];
 
   const langs: ILang[] = [
     {
       id: 1,
       name: "uz",
-      value: "uz"
+      value: "uz",
     },
     {
       id: 2,
       name: "ru",
-      value: "ru"
+      value: "ru",
     },
     {
       id: 2,
       name: "kr",
-      value: "kr"
+      value: "kr",
     },
-  ]
+  ];
 
   return (
-    <div className='header-wrapper container'>
-      {mobileSearchModal &&
+    <div className="header-wrapper container">
+      {mobileSearchModal && (
         <div className="search-modal">
           <div className="search-modal__top">
-            <div className="icon-btn back-btn mr-[10px]" onClick={() => showMobileSearchModal(false)}>
+            <div
+              className="icon-btn back-btn mr-[10px]"
+              onClick={() => showMobileSearchModal(false)}
+            >
               <img src={BackIcon} alt="burger" className="burger-icon" />
             </div>
-            <Input autoFocus={mobileSearchModal && true} className='header-searchbar' placeholder={t("Mahsulotlarni izlash")} type="text" />
+            <Input
+              autoFocus={mobileSearchModal && true}
+              className="header-searchbar"
+              placeholder={t("Mahsulotlarni izlash")}
+              type="text"
+            />
           </div>
-          <div className="search-modal__bottom">
-            
-          </div>
+          <div className="search-modal__bottom"></div>
         </div>
-      }
-      <div className='flex items-center'>
+      )}
+      <div className="flex items-center">
         <div className="icon-btn burger-btn mr-[16px]">
           <img src={BurgerIcon} alt="burger" className="burger-icon" />
         </div>
@@ -97,22 +103,29 @@ const Header = () => {
       </div>
       <div className="header-navbar">
         <ul className="header-navbar__list">
-          {!navBarState ?
-            navItems.map(menu => (
+          {!navBarState ? (
+            navItems.map((menu) => (
               <li className="header-navbar__list-item">
-                <Link to={get(menu, "link")}>
-                  {t(get(menu, "title"))}
-                </Link>
+                <Link to={get(menu, "link")}>{t(get(menu, "title"))}</Link>
               </li>
-            )) :
-            <Input autoFocus={navBarState && true} className='header-searchbar' placeholder={t("Mahsulotlarni izlash")} type="text" />
-          }
-          <button onClick={() => seacrhBarState(prev => !prev)} className='header-navbar__search-btn'>
+            ))
+          ) : (
+            <Input
+              autoFocus={navBarState && true}
+              className="header-searchbar"
+              placeholder={t("Mahsulotlarni izlash")}
+              type="text"
+            />
+          )}
+          <button
+            onClick={() => seacrhBarState((prev) => !prev)}
+            className="header-navbar__search-btn"
+          >
             <img src={SearchIcon} alt="lens-icon" className="search-icon" />
           </button>
         </ul>
       </div>
-      <div className='flex'>
+      <div className="flex">
         <Select
           suffixIcon={<Arrow2 />}
           className="lang-select"
@@ -122,13 +135,16 @@ const Header = () => {
             // changeLanguage(value);
           }}
         >
-          {langs.map(lang => (
+          {langs.map((lang) => (
             <Option value={get(lang, "value")}>
               <p>{t(get(lang, "name"))}</p>
             </Option>
           ))}
         </Select>
-        <div className="icon-btn search-btn" onClick={() => showMobileSearchModal(true)}>
+        <div
+          className="icon-btn search-btn"
+          onClick={() => showMobileSearchModal(true)}
+        >
           <img src={SearchIcon} alt="cart" className="search-icon" />
         </div>
         <div className="icon-btn cart-btn">
@@ -136,7 +152,7 @@ const Header = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Layout } from "antd";
 
-import Header from 'components/header'
+import Header from "components/header";
+import Footer from "components/footer";
 
 const { Content } = Layout;
 
 const App: React.FC = () => {
+  const location = useLocation();
   return (
     <Layout className="h-full">
-      <Header/>
+      <Header />
       <Layout>
         <Content className="bg-[#fff]">
           <Outlet />
         </Content>
       </Layout>
+      {location.pathname === "/contact" ? <div></div> : <Footer />}
     </Layout>
   );
 };
