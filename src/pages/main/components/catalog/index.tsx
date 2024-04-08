fd2debde9bfb5f30ff4cb1977e2a6e7b0ab59a27
otabekov-komron <mrOK.blog@gmail.com>
2024-04-08 12:57:15 +0500

    feat: Gsap added

--- src/pages/main/components/catalog/index.tsx
+++ src/pages/main/components/catalog/index.tsx
@@ -1,44 +1,50 @@
-import { useState } from 'react'
-import { useHooks } from 'hooks'
-import { CatalogCard } from 'components';
-import { categories, products } from 'mock';
+import { useState } from "react";
+import { useHooks } from "hooks";
+import { CatalogCard } from "components";
+import { categories, products } from "mock";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import './style.scss'
+import "swiper/css";
+import "swiper/css/pagination";
+import "./style.scss";
+import { HeroIcon1, HeroIcon2, HeroIcon3 } from "assets/images/icons";
 
 const Catalog = () => {
-
   const [selectedCategory, setSelectedCategory] = useState({
     id: 1,
-    title: "Antioksidantlar"
-  },)
+    title: "Antioksidantlar",
+  });
 
-  const { t, get } = useHooks()
+  const { t, get } = useHooks();
 
   return (
-    <div className='catalog-section'>
+    <div className="catalog-section">
       <h2 className="catalog-heading">{t("Katalog")}</h2>
       <div className="catalog-categories">
         {categories.map((category) => (
-          <button className={get(selectedCategory, "id") == get(category, "id") ? 'selectedCategory category-btn' : 'category-btn'} onClick={() => setSelectedCategory(category)} key={get(category, "id")}>
+          <button
+            className={
+              get(selectedCategory, "id") == get(category, "id")
+                ? "selectedCategory category-btn"
+                : "category-btn"
+            }
+            onClick={() => setSelectedCategory(category)}
+            key={get(category, "id")}
+          >
             {get(category, "title")}
           </button>
         ))}
 
         <div className="catalog-list">
-          {
-            products.map((item) => (
-              <CatalogCard {...{ item }} />
-            ))
-          }
+          {products.map((item) => (
+            <CatalogCard {...{ item }} />
+          ))}
         </div>
       </div>
-      <div className='flex justify-center items-center'>
-        <button className='view-more'>{t("Yana ko’rish")}</button>
+      <div className="flex justify-center items-center">
+        <button className="view-more">{t("Yana ko’rish")}</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;
