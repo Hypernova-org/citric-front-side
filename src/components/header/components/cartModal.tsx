56b6778eccd8b55e8c846cc866991c7306c5bdde
otabekov-komron <mrOK.blog@gmail.com>
2024-04-15 18:07:44 +0500

    feat: something new

--- src/components/header/components/cartModal.tsx
+++ src/components/header/components/cartModal.tsx
@@ -1,88 +1,114 @@
-import { useEffect } from 'react';
-import { notification } from 'antd';
+import { useEffect } from "react";
+import { notification } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
-import Container from 'modules/container';
-import { useHooks } from 'hooks';
+import Container from "modules/container";
+import { useHooks } from "hooks";
 import useStore from "store";
-import { Field } from 'formik';
-import { Fields } from 'components';
+import { Field } from "formik";
+import { Fields } from "components";
 
 import CloseIcon from "assets/images/icons/close.svg";
 
-import '../style.scss'
+import "../style.scss";
+import { MinusIcon, PlusIcon } from "assets/images/icons";
 
 const CartModal = ({ cartModal, showCartModal }: any) => {
   const { basket, removeFromBasket, updateQuantity } = useStore();
-  const { t, get } = useHooks()
+  const { t, get } = useHooks();
 
   useEffect(() => {
     if (cartModal) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     }
 
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, [cartModal]);
 
   const basketdata = basket.map((curr) => {
-    return (
-      {
-        id: get(curr, "product._id"),
-        amount: curr["quantity"]
-      }
-    );
+    return {
+      id: get(curr, "product._id"),
+      amount: curr["quantity"],
+    };
   }, []);
 
   return (
     <div className={`cart-modal`}>
-      <div className={`modal-overlay ${cartModal ? 'show' : ''}`} onClick={() => showCartModal(false)} />
-      <div className={`modal-container ${cartModal ? 'show' : ''}`}>
+      <div
+        className={`modal-overlay ${cartModal ? "show" : ""}`}
+        onClick={() => showCartModal(false)}
+      />
+      <div className={`modal-container ${cartModal ? "show" : ""}`}>
         <div className="modal-content">
-          <div className='modal-top-section'>
+          <div className="modal-top-section">
             <div className="top-part">
               <p className="cart-modal__title">{t("Sotib olish")}</p>
               <span className="close-btn" onClick={() => showCartModal(false)}>
                 <img src={CloseIcon} alt="citric.uz" className="close-icon" />
               </span>
             </div>
-            <p className='cart-modal__subtitle'>{t(`Mahsulotni sotib olish uchun ma’lumotlaringizni qoldiring tez siz bilan bog’lanamiz`)}</p>
+            <p className="cart-modal__subtitle">
+              {t(
+                `Mahsulotni sotib olish uchun ma’lumotlaringizni qoldiring tez siz bilan bog’lanamiz`
+              )}
+            </p>
           </div>
           <div className="modal-mid-section">
             {basket.map((item: any) => {
-              const product = get(item, "product")
+              const product = get(item, "product");
 
               return (
                 <div key={get(product, "_id")} className="cart-modal__item">
-                  <div className='flex'>
-                    <div className="left-side"><img className='object-cover' src={get(product, "images[0].small")} alt="citric.uz" /></div>
+                  <div className="flex">
+                    <div className="left-side">
+                      <img
+                        className="object-cover"
+                        src={get(product, "images[0].small")}
+                        alt="citric.uz"
+                      />
+                    </div>
                     <div className="right-side">
-                      <p>
-                        {get(product, "productTitle")}
-                      </p>
-                      <div className='cart-amount-controller'>
-                        <span className='minus-amount'
+                      <p>{get(product, "productTitle")}</p>
+                      <div className="cart-amount-controller">
+                        <span
+                          className="minus-amount"
                           onClick={() => {
                             updateQuantity(
                               get(product, "_id"),
                               Math.max(1, item.quantity - 1)
-                            )
+                            );
                           }}
-                        >-</span>
-                        <p className='counter-amount'>{get(item, "quantity")}</p>
-                        <span className='plus-amount'
+                        >
+                          <MinusIcon />
+                        </span>
+                        <p className="counter-amount">
+                          {get(item, "quantity")}
+                        </p>
+                        <span
+                          className="plus-amount"
                           onClick={() =>
-                            updateQuantity(get(product, "_id"), item.quantity + 1)
+                            updateQuantity(
+                              get(product, "_id"),
+                              item.quantity + 1
+                            )
                           }
-                        >+</span>
+                        >
+                          <PlusIcon />
+                        </span>
                       </div>
                     </div>
                   </div>
-                  <div className='delete-icon' onClick={() => removeFromBasket(get(product, "_id"))}><DeleteOutlined /></div>
+                  <div
+                    className="delete-icon"
+                    onClick={() => removeFromBasket(get(product, "_id"))}
+                  >
+                    <DeleteOutlined />
+                  </div>
                 </div>
-              )
+              );
             })}
           </div>
           <div className="modal-bottom-section">
@@ -103,11 +129,11 @@ const CartModal = ({ cartModal, showCartModal }: any) => {
                   name: "products",
                   type: "array",
                   value: basketdata,
-                }
+                },
               ]}
               onSuccess={(data, resetForm, query) => {
-                resetForm()
-                localStorage.removeItem('basket')
+                resetForm();
+                localStorage.removeItem("basket");
                 notification["success"]({
                   message: data ? "Успешно изменен!" : "Успешно!",
                   duration: 2,
@@ -124,8 +150,10 @@ const CartModal = ({ cartModal, showCartModal }: any) => {
               {() => {
                 return (
                   <div>
-                    <div className='relative'>
-                      <label className='payment-input-label'>{t("Ismingiz")}</label>
+                    <div className="relative">
+                      <label className="payment-input-label">
+                        {t("Ismingiz")}
+                      </label>
                       <Field
                         component={Fields.Input}
                         className="payment-input mb-[12px]"
@@ -135,8 +163,10 @@ const CartModal = ({ cartModal, showCartModal }: any) => {
                         size="large"
                       />
                     </div>
-                    <div className='relative'>
-                      <label className='payment-input-label'>{t("Telefon raqamingiz")}</label>
+                    <div className="relative">
+                      <label className="payment-input-label">
+                        {t("Telefon raqamingiz")}
+                      </label>
                       <Field
                         component={Fields.Input}
                         className="payment-input"
@@ -146,7 +176,9 @@ const CartModal = ({ cartModal, showCartModal }: any) => {
                         size="large"
                       />
                     </div>
-                    <button type='submit' className='submit-form-btn'>{t("Ariza qoldirish")}</button>
+                    <button type="submit" className="submit-form-btn">
+                      {t("Ariza qoldirish")}
+                    </button>
                   </div>
                 );
               }}
@@ -155,7 +187,7 @@ const CartModal = ({ cartModal, showCartModal }: any) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartModal
+export default CartModal;
