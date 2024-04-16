ec20c2cefb484dc8030fe80e62f96912da033d83
Doniyor <doni404error@gmail.com>
2024-04-16 15:12:21 +0500

    fix: t in comment card

--- src/components/commentCard/index.tsx
+++ src/components/commentCard/index.tsx
@@ -1,11 +1,13 @@
 import { Comment } from "assets/images/icons";
+import { useHooks } from "hooks";
 
 const CommentCard = (props: any) => {
+  const { t } = useHooks()
   return (
     <div className="comment_item cursor-pointer" onClick={props.onClick}>
       <Comment />
       <p className="comment_desc">{props.description}</p>
-      <p className="comment_more">Batafsil ko'rish</p>
+      <p className="comment_more">{t("Batafsil ko'rish")}</p>
       <div className="comment_author">
         <img
           className="comment_author__img"
